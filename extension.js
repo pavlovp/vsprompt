640407a9eef0ce681f3d9584580850dbaa53987e
pavlovp <pavel.pavlov1990@gmail.com>
2023-04-23 18:50:45 +0200

    wip

--- extension.js
+++ extension.js
@@ -1,8 +1,48 @@
 const vscode = require('vscode');
-const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const { Configuration, OpenAIApi } = require("openai");
+
+let openAIButton;
+
+async function invokeOpenAI(prompt) {
+  const openaiApiKey = vscode.workspace.getConfiguration().get('openai.apiKey');
+  const openaiModel = vscode.workspace.getConfiguration().get('openai.model') || "text-davinci-003";
+  const openaiTemperature = vscode.workspace.getConfiguration().get('openai.temperature') || 0.7;
+  const openaiMaxTokens = vscode.workspace.getConfiguration().get('openai.maxTokens') || 1000;
+  const openaiTopP = vscode.workspace.getConfiguration().get('openai.topP') || 1;
+  const openaiFrequencyPenalty = vscode.workspace.getConfiguration().get('openai.frequencyPenalty') || 0;
+  const openaiPresencePenalty = vscode.workspace.getConfiguration().get('openai.presencePenalty') || 0;
+
+  if (!openaiApiKey) {
+    vscode.window.showErrorMessage('OpenAI API key not set. Please set it in the extension settings.');
+    await vscode.commands.executeCommand('workbench.action.openSettings', 'openai.apiKey');
+    vscode.env.openExternal('https://platform.openai.com/account/api-keys');
+    return '';
+  }
+
+  try {
+    const configuration = new Configuration({
+      apiKey: openaiApiKey,
+    });
+    const openai = new OpenAIApi(configuration);
+    const completion = await openai.createCompletion({
+      model: openaiModel,
+      prompt: prompt,
+      temperature: openaiTemperature,
+      max_tokens: openaiMaxTokens,
+      top_p: openaiTopP,
+      frequency_penalty: openaiFrequencyPenalty,
+      presence_penalty: openaiPresencePenalty,
+    });
+    const textResp = completion.data.choices[0].text;
+
+    return textResp;
+  } catch (error) {
+    vscode.window.showErrorMessage(`OpenAI API error: ${error.message}`);
+    return '';
+  }
+}
 
 function openNewFileInSplitView(filePath) {
   const uri = vscode.Uri.parse(`file:///${filePath}`);
@@ -13,32 +53,16 @@ function openNewFileInSplitView(filePath) {
   vscode.window.showTextDocument(uri, options);
 }
 
-let openaiApiKey;
 function activate(context) {
-  // Check if OpenAI API key is set
-  openaiApiKey = vscode.workspace.getConfiguration().get('openai.apiKey');
-  if (!openaiApiKey) {
-    vscode.window.showInformationMessage(
-      'OpenAI API key not set. Please set it in the extension settings.'
-    );
-    vscode.env.openExternal('https://platform.openai.com/account/api-keys');
-    return;
-  }
-
-
-  let openAIButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-
+  openAIButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
   openAIButton.text = '$(cpu) Complete with OpenAI (ctrl+shift+a)';
   openAIButton.tooltip = 'Complete with OpenAI';
   openAIButton.command = 'extension.invokeOpenAI';
   openAIButton.show();
-
   context.subscriptions.push(openAIButton);
 
-  // Add command to invoke OpenAI API
   context.subscriptions.push(
     vscode.commands.registerCommand('extension.invokeOpenAI', async () => {
-      // Get active file contents
       const activeEditor = vscode.window.activeTextEditor;
       if (!activeEditor) {
         vscode.window.showInformationMessage('No file open.');
@@ -46,41 +70,24 @@ function activate(context) {
       }
       const prompt = activeEditor.document.getText();
 
-      // Invoke OpenAI API
-
-
       const result = await invokeOpenAI(prompt);
 
-      // Save result to new file
-      const fileName = path.basename(activeEditor.document.fileName);
-      const dirName = path.dirname(activeEditor.document.fileName);
-      const resultFileName = path.join(dirName, `completion-${fileName}`);
-      fs.writeFileSync(resultFileName, result);
+      if (result) {
+        const fileName = path.basename(activeEditor.document.fileName);
+        const dirName = path.dirname(activeEditor.document.fileName);
+        const newFileName = getNewFileName(fileName);
+        const resultFileName = path.join(dirName, newFileName);
 
-      openNewFileInSplitView(resultFileName);
-
-      vscode.window.showInformationMessage(`Result saved to ${resultFileName}.`);
+        fs.writeFileSync(resultFileName, result);
+        openNewFileInSplitView(resultFileName);
+        vscode.window.showInformationMessage(`Result saved to ${resultFileName}.`);
+      }
     })
   );
 }
 
-async function invokeOpenAI(prompt) {
-  try {
-    const configuration = new Configuration({
-      apiKey: openaiApiKey,
-    });
-    const openai = new OpenAIApi(configuration);
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: prompt,
-    });
-    const textResp = completion.data.choices[0].text;
-
-    return textResp;
-  } catch (error) {
-    vscode.window.showErrorMessage(`OpenAI API error: ${error.message}`);
-    return '';
-  }
+function getNewFileName(fileName) {
+  return `completion-${fileName}`
 }
 
 exports.activate = activate;
